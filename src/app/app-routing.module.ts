63e13673504e5a6e274f014ea83519ba27d533ba
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-04-20 14:12:04 +0200

    delete components detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
-import { DetailComponent } from './components/detail/detail.component';
 
 
 const routes: Routes = [
   { path: 'list', component: ListComponent },
-  { path: 'detail', component: DetailComponent },
   { path: '', redirectTo:'/list', pathMatch:'full'}
 ];
 
