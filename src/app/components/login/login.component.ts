4187cb0d0dd64108a9cc061b5538a36985ebde4f
contilorenzo <lorenzo.conti.bg@gmail.com>
2020-04-22 16:13:32 +0200

    Css login finiti

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,13 +11,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   usersList: UsersProfile[];
-  
   loginForm: FormGroup;
 
-  constructor(private fb : FormBuilder, private loginService : LoginService) {
-    this.loginForm=this.fb.group({
-      username : "",
-      password : ""
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      username : '',
+      password : ''
     })
   }
 
@@ -28,5 +27,5 @@ export class LoginComponent implements OnInit {
   }
  // login() {
   // this.loginService.eseguiLogin(this.loginForm.value);
-  //}
+  // }
 }
