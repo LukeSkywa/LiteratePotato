dac3ba6e6d9bbfc9f69fd6f2283c2d7a26b76a00
contilorenzo <lorenzo.conti.bg@gmail.com>
2020-04-22 12:05:25 +0200

    feedback impostato, login quasi completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { EditGuardService } from './services/edit-guard.service';
-import { GuardService } from './services/guard.service';
 import { RegistrazioneComponent } from './components/registration/registration.component';
 import { ListComponent } from './components/list/list.component';
 import { HomeComponent } from './components/home/home.component';
-//questi sono i routes base man mano che create le cose e vi servono scommentate queste qua sotto e modificatele a piacimento
+import { FeedbackComponent } from './components/feedback/feedback.component';
+
+
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
-      { path: 'registrati', component:RegistrazioneComponent},
-      { path: 'list', component: ListComponent },
-      { path: 'home', component: HomeComponent },
-      { path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: 'registrati', component: RegistrazioneComponent},
+    { path: 'list', component: ListComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'feedback', component: FeedbackComponent },
+
+    { path: '', redirectTo: '/login', pathMatch: 'full'},
 
 ];
-
-
 
 @NgModule ({
 
@@ -24,6 +24,8 @@ const routes: Routes = [
   exports: [RouterModule]
 
 })
-export class AppRoutingModule {
-  
- }
+
+export class AppRoutingModule { }
+
+
+
